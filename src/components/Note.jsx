441b46ebeae7f49e17a2a91f83d17e3852ce72d9
Chad Buntrakulsuk <chad.buntrakulsuk@gmail.com>
2022-07-09 11:11:47 -0700

    add completed functionality

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,21 @@
+import {useState} from 'react';
 
 function Note (props) {
+
+    const [isCompleted, setisCompleted] = useState(false);
+
+    const strikeThough = {
+        textDecoration: 'line-through',
+        textDecorationColor: '#9f1a46'
+    }
+
+    function handleClick () {
+        setisCompleted(true);
+    }
+
     return (
-        <div className="note">
-            <p>{props.content}</p>
+        <div style={isCompleted ? {...strikeThough} : null} onClick={handleClick} className="note" >
+            {props.content}
         </div>
     );
 } 
